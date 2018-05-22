5ee0b7eff5bba50eeaffe5ef5a01695a4ead81f9
Unknown <andrea3.0@hotmail.it>
2018-05-22 10:29:10 +0200

    start button implementato e messo nei livelli

---
+++ Bounce Test/Assets/Scripts/StartButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
+    public GameObject platform;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        platform.SetActive(false);
+        this.gameObject.GetComponent<AudioSource>().Play();
+        this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 200);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 175);
+    }
+}
